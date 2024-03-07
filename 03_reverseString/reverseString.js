1f39c297dec66b95ecec8c7743180607586fe8a1
Lily W <lilywu256@gmail.com>
2024-03-07 11:29:48 +0900

    Upload solutions for exercises 1-4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let message = '';
+    for (let i = 0; i < string.length; i++) {
+        negative = string.length - 1 - i;
+        message += string[negative];
+    }
+    return message;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...args) {
+    let newArray = [];
+    for (let i = 0; i < inputArray.length; i++) {
+       if (!args.includes(inputArray[i])) newArray.push(inputArray[i]);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
