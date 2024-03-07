0a066f0c2f7fc9178930f4fa828df5a4c541d960
Lily W <lilywu256@gmail.com>
2024-03-07 12:12:05 +0900

    Upload solutions for exercises 5-7
    Finished assignment for Array and Loops unit of the Foundations
    course!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ((typeof num1 != "number") || (typeof num2 != "number")) return "ERROR";
+    if ((num1 < 0)|| (num2 < 0)) return "ERROR";
 
+    let lower, upper = 0;
+    if (num1 < num2) {
+        lower = num1;
+        upper = num2;
+    }
+    else {
+        lower = num2;
+        upper = num1;
+    }
+
+    let sum = 0;
+    for (let i = lower; i <= upper; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
